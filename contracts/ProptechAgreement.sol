1994017b4fc916dddfa8717f2e1f75bb0dacede7
pedro bufulin <pedro.almeida@ufu.br>
2023-03-29 23:13:21 -0300

    agreement contract, test contract

--- contracts/ProptechAgreement.sol
+++ contracts/ProptechAgreement.sol
@@ -5,20 +5,16 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact pedrohba18@gmail.com
-contract ProptechAgreement is ERC721, Ownable {
+contract ProptechAgreement is ERC721 {
     constructor() ERC721("ProptechAgreement", "DAC") {
-
+        _safeMint(msg.sender, 1);
     }
-'
+
     function _baseURI() override internal view virtual returns (string memory) {
         return "bafybeid5bk5psrbbgk3m2eft47blcy4q4rkln4zgyhtbkpqbtpy2mc5hjy";
     }
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
-    }
-
-    function version(){
-        return "0.1.0"
+    function version()  public pure returns (string memory) { 
+        return "0.1.0";
     }
 }
