4a1044276432d6526bd9ee5deeb4adc9884dc56b
pedro bufulin <pedro.almeida@ufu.br>
2023-03-30 12:59:01 -0300

    feat: HolidayClubToken

---
+++ contracts/HolidayClubToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract HolidayClubToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC1155_init("HolidayClubToken");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    function version()  public pure returns (string memory) { 
+        return "0.1.0";
+    }
+}
